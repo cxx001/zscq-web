2161d5baf2084697b962cd516842b1e5ff4f42e1
chenxiaoxian <469350450@qq.com>
2017-02-22 19:23:08 +0800

    111

---
+++ src/resource.js
@@ -0,0 +1,12 @@
+var res = {
+    BackGround_png : "res/background.png",
+    Start_N_png : "res/start_N.png",
+    Start_S_png : "res/start_S.png",
+    Sushi_plist : "res/sushi.plist",
+    Sushi_png : "res/sushi.png"
+};
+
+var g_resources = [];
+for (var i in res) {
+    g_resources.push(res[i]);
+}
